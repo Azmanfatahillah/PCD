2f366291dfb04942f9830b729c8946ad04522f80
Azmanfatahillah <azman.fatahillah1@gmail.com>
2018-06-02 06:30:56 +0700

    Add files via upload

--- train.py
+++ train.py
@@ -1,23 +1,26 @@
-import h5py
+# import the necessary packages
 import numpy as np
 import os
-import glob
 import cv2
-from matplotlib import pyplot
+import glob
+import csv
+import pandas
 from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.model_selection import KFold, StratifiedKFold
 from sklearn.metrics import confusion_matrix, accuracy_score, classification_report
-from sklearn.linear_model import LogisticRegression
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
-from sklearn.externals import joblib
+import matplotlib.pyplot as plt
+import csv23
 
-# no.of.trees for Random Forests
-num_trees = 100
+
+# fixed-sizes for image
+fixed_size = tuple((300,300))
+
+# path to training data
+train_path = "./coba/"
+
+# bins for histogram
+bins = 8
 
 # train_test_split size
 test_size = 0.10
@@ -25,111 +28,51 @@ test_size = 0.10
 # seed for reproducing same results
 seed = 9
 
-# create all the machine learning models
-models = []
-# models.append(('LR', LogisticRegression(random_state=9)))
-# models.append(('LDA', LinearDiscriminantAnalysis()))
-# models.append(('KNN', KNeighborsClassifier()))
-# models.append(('CART', DecisionTreeClassifier(random_state=9)))
-models.append(('RF', RandomForestClassifier(n_estimators=num_trees, random_state=9)))
-# models.append(('NB', GaussianNB()))
-# models.append(('SVM', SVC(random_state=9)))
-
-# variables to hold the results and names
-results = []
-names = []
-scoring = "accuracy"
-
-# import the feature vector and trained labels
-h5f_data = h5py.File('output/data.h5', 'r')
-h5f_label = h5py.File('output/labels.h5', 'r')
-
-global_features_string = h5f_data['dataset_1']
-global_labels_string = h5f_label['dataset_1']
-
-global_features = np.array(global_features_string)
-global_labels = np.array(global_labels_string)
-
-h5f_data.close()
-h5f_label.close()
-
-# verify the shape of the feature vector and labels
-print "[STATUS] features shape: {}".format(global_features.shape)
-print "[STATUS] labels shape: {}".format(global_labels.shape)
-
-print "[STATUS] training started..."
-
-
-
-# split the training and testing data
-(trainDataGlobal, testDataGlobal, trainLabelsGlobal, testLabelsGlobal) = train_test_split(np.array(global_features),
-                                                                                          np.array(global_labels),
-                                                                                          test_size=test_size,
-                                                                                          random_state=seed)
-
-print "[STATUS] splitted train and test data..."
-print "Train data  : {}".format(trainDataGlobal.shape)
-print "Test data   : {}".format(testDataGlobal.shape)
-print "Train labels: {}".format(trainLabelsGlobal.shape)
-print "Test labels : {}".format(testLabelsGlobal.shape)
-
-
-
-
-# filter all the warnings
-# import warnings
-# warnings.filterwarnings('ignore')
-
-# 10-fold cross validation
-for name, model in models:
-    kfold = KFold(n_splits=10, random_state=7)
-    cv_results = cross_val_score(model, trainDataGlobal, trainLabelsGlobal, cv=kfold, scoring=scoring)
-    results.append(cv_results)
-    names.append(name)
-    msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
-    print(msg)
-
-# boxplot algorithm comparison
-fig = pyplot.figure()
-fig.suptitle('Machine Learning algorithm comparison')
-ax = fig.add_subplot(111)
-pyplot.boxplot(results)
-ax.set_xticklabels(names)
-pyplot.show()
-
-
-# '''
-import matplotlib.pyplot as plt
-import mahotas
-
-# bins for histogram
-bins = 8
-
-# fixed-sizes for image
-fixed_size = tuple((100, 100))
-
-# path to training data
-train_path = "fruits-360\\Training"
-
 # get the training labels
 train_labels = os.listdir(train_path)
 
-# feature-descriptor-1: Hu Moments
+# sort the training labels
+train_labels.sort()
+# print(train_labels)
+
+#=====================================================================
+
+filename = open('cannypixel.csv', 'r')
+dataframe = pandas.read_csv(filename)
+
+kelas = dataframe.drop(dataframe.columns[:-1], axis=1)
+data = dataframe.drop(dataframe.columns[-1:], axis=1)
+
+print(data)
+print(kelas)
+
+# empty lists to hold feature vectors and labels
+global_features = []
+labels = []
+
+i, j = 0, 0
+k = 0
+
 def fd_hu_moments(image):
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     feature = cv2.HuMoments(cv2.moments(image)).flatten()
     return feature
 
-# feature-descriptor-2: Haralick Texture
-def fd_haralick(image):
-    # convert the image to grayscale
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    # compute the haralick texture feature vector
-    haralick = mahotas.features.haralick(gray).mean(axis=0)
-    # return the result
-    return haralick
 
-# feature-descriptor-3: Color Histogram
+def canny(image):
+	image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	edges = cv2.Canny(image,20,255,L2gradient=False)  #edges = cv2.Canny(im,lower_threshold,upper_threshold,L2gradient=False)
+	return edges
+
+
+#def fd_haralick(image):
+    # convert the image to grayscale
+ #   gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # compute the haralick texture feature vector
+ #   haralick = mahotas.features.haralick(gray).mean(axis=0)
+    # return the result
+  #  return haralick
+
 def fd_histogram(image, mask=None):
     # convert the image to HSV color-space
     image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
@@ -140,20 +83,41 @@ def fd_histogram(image, mask=None):
     # return the histogram
     return hist.flatten()
 
-# create the model - Random Forests
-clf  = RandomForestClassifier(n_estimators=100, random_state=9)
-# clf = LinearDiscriminantAnalysis()
-# clf = LogisticRegression(random_state=9)
-# clf = DecisionTreeClassifier(random_state=9)
+num = 102
 
+# create all the machine learning models
+models = []
+models.append(('SVM', SVC(random_state=9)))
+
+# variables to hold the results and names
+results = []
+names = []
+
+# filter all the warnings
+import warnings
+warnings.filterwarnings('ignore')
+
+# 10-fold cross validation
+for name, model in models:
+    kfold = KFold(n_splits=10, random_state=7)
+    cv_results = cross_val_score(model, data, kelas, cv=kfold, scoring=scoring)
+    results.append(cv_results)
+    names.append(name)
+    msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+    print(msg)
+
+
+
+# create the model - Random Forests
+clf  =  SVC(random_state=num)
 # fit the training data to the model
-clf.fit(trainDataGlobal, trainLabelsGlobal)
-# clf.
+clf.fit(data,kelas)
+
 # path to test data
-test_path = "fruits-360\\Validation"
+test_path = "./training/"
 
 # loop through the test images
-for file in glob.glob(test_path + "\\*.jpg"):
+for file in glob.glob(test_path + "/*.jpg"):
     # read the image
     image = cv2.imread(file)
 
@@ -164,26 +128,23 @@ for file in glob.glob(test_path + "\\*.jpg"):
     # Global Feature extraction
     ####################################
     fv_hu_moments = fd_hu_moments(image)
-    fv_haralick   = fd_haralick(image)
+    fv_canny      = canny(image)
     fv_histogram  = fd_histogram(image)
 
     ###################################
     # Concatenate global features
     ###################################
-    global_feature = np.hstack([fv_histogram, fv_haralick, fv_hu_moments])
+    global_feature = np.hstack([fv_histogram, fv_canny, fv_hu_moments])
 
     # predict label of test image
     prediction = clf.predict(global_feature.reshape(1,-1))[0]
-    # prediction = clf.predict(global_feature.reshape(1,-1))[0]
+    cv2.putText(image,prediction, (20,30), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0,255,255), 3)
 
-    # show predicted label on image
-    cv2.putText(image, train_labels[prediction], (10,80), cv2.FONT_HERSHEY_PLAIN, 0.8, (0,0,0), 1)
-    # cv2.putText(image,)
+
     # display the output image
     plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
     plt.show()
-    cv2.imwrite('output\hasil.jpg', image)
 
-cv2.waitKey(1)
-cv2.destroyAllWindows()
-# '''
+
+
+
